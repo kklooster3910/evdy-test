8745c316a02b0e7e97750bc23f2035b36b3decb7
Ken Klooster <kklooster3910@gmail.com>
2019-11-07 12:04:38 -0500

    add memorials sorted by date

--- src/components/memorials/memorial_index.jsx
+++ src/components/memorials/memorial_index.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router";
 
 class MemorialIndex extends React.Component {
   constructor(props) {
     super(props);
+    // this.formatDate = this.formatDate.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchMemorials();
   }
 
+  formatDate(date) {
+    return new Date(date).toDateString();
+  }
+
+  sortByDate(memorialArray) {
+    const sortedMemorialDatesArray = memorialArray.map(memorial => ({
+      creationDate: memorial.creationDate,
+      deceased: memorial.name.last + ", " + memorial.name.first
+    }));
+
+    sortedMemorialDatesArray.sort((a, b) => (a.creationDate > b.creationDate) ? 1 : -1);
+
+    return sortedMemorialDatesArray.map(memorial => (
+      <li>
+        Memorial Creation Date: {this.formatDate(memorial.creationDate) + "   "}
+        <br/>
+        Name (Last Name First): {memorial.deceased}
+      </li>
+    ));
+
+  }
+
   render() {
     if (this.props.memorials.length === 0) {
       return <span>"Loading Fam..."</span>;
     }
-    // debugger;
-    const memorialArray = this.props.memorials[0].map( memorial => (
-      <li key={memorial._id}>{new Date(memorial.creationDate).toDateString()}</li>
-    ))
 
-    return (
-    <ul>
-      {memorialArray}
-    </ul>)
+    const memorialSortedByDate = this.sortByDate(this.props.memorials[0]);
+
+    return <ul>{memorialSortedByDate}</ul>;
   }
 }
-
 
 export default withRouter(MemorialIndex);
