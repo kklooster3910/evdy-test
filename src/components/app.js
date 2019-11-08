0abe4fc7c9312f68f72dfb86f1b93d81ef45fcf8
Ken Klooster <kklooster3910@gmail.com>
2019-11-08 13:43:03 -0500

    add readme documentation and screenshots

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import MemorialIndex from './memorials/memorial_container'
-// import logo from './logo.svg';
-// import './App.css';
 
 const App = () => (
   <div className='main-app'>
--- src/index.js
+++ src/index.js
@@ -5,17 +5,7 @@ import configureStore from './store/store';
 import './index.scss';
 // import './nested_css.scss'
 
-// import App from './components/app';
-// import * as serviceWorker from './serviceWorker';
-
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('root');
   ReactDOM.render(<Root store={configureStore({})}/>, rootElement)
 })
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
